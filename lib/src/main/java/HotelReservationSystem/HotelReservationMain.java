013484b39eeab4d0fe699e7bbd01dcc09f04edca
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-19 08:29:40 +0530

    Initialized the hotel reservation system program

---
+++ lib/src/main/java/HotelReservationSystem/HotelReservationMain.java
@@ -0,0 +1,10 @@
+package HotelReservationSystem;
+
+public class HotelReservationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to hotel reservation system");
+
+	}
+
+}
